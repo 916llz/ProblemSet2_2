7d57ca62e673d3d7eed8ba7895e8ebaa43781323
ALUNO <ALUNO@10.85.198.5>
2024-06-04 16:26:51 -0300

    Commit project ProblemSet2_2

---
+++ src/ProblemSet2_2.java
@@ -0,0 +1,14 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting. 
+        System.out.println("   ______ ");
+        System.out.println(" _        _ ");
+        System.out.println("_  -    -  _ ");
+        System.out.println("_  0    0  _ ");
+        System.out.println("_          _ ");
+        System.out.println("_   ____   _ ");
+        System.out.println(" _        _ ");
+        System.out.println("  _______ ");
+        }
+    }
